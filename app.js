56e707b608d00b0133e65c48df9b68ae25b9e7ff
Max Wendt <mxwendt@gmail.com>
2015-10-29 11:33:58 +0100

    .

--- app.js
+++ app.js
@@ -10,10 +10,28 @@ var three = THREE.Bootstrap(options);
 var eyeOrigin = three.argon.objectFromEntity(context.eyeOrigin);
 var root = new THREE.Object3D();
 
-addRandomImage(-1000);
-addRandomImage(1000);
+addPlane(-1000);
+addPlane(1000);
+// addRandomImage(-1000);
+// addRandomImage(1000);
 
 eyeOrigin.add(root);
+
+function addPlane(z) {
+  var element = document.createElement('div');
+  element.className = 'plane';
+  // element.style.backgroundColor = 'rgba(0,127,127,' + ( Math.random() * 0.5 + 0.25 ) + ')';
+  element.style.backgroundColor = 'rgba(0,127,127,1)';
+
+  var object = new THREE.CSS3DObject(element);
+  object.matrixAutoUpdate = false;
+
+  plane.position.x = 0;
+  plane.position.y = 0;
+  plane.position.z = z;
+
+  root.add(object);
+}
 
 // Creates a plane with an image to place in space
 function addRandomImage(z) {
