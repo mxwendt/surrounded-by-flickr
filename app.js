d94f3c998848ab39527759320657845adb219f04
Max Wendt <mxwendt@gmail.com>
2015-10-29 11:24:01 +0100

    .

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ var options = THREE.Bootstrap.createArgonOptions(context);
 options.renderer = {klass: THREE.CSS3DRenderer};
 var three = THREE.Bootstrap(options);
 var eyeOrigin = three.argon.objectFromEntity(context.eyeOrigin);
-
-// Create a plane with an image to place in space
-var geometry = new THREE.PlaneGeometry(16, 9, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide});
-var plane = new THREE.Mesh(geometry, material);
-plane.position.x = 0;
-plane.position.y = 0;
-plane.position.z = -1000;
-
 var root = new THREE.Object3D();
 
-root.add(plane);
+addRandomImage(-1000);
+addRandomImage(1000);
 
 eyeOrigin.add(root);
+
+// Creates a plane with an image to place in space
+function addRandomImage(z) {
+  var geometry = new THREE.PlaneGeometry(16, 9, 1, 1);
+  var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide});
+  var plane = new THREE.Mesh(geometry, material);
+  plane.position.x = 0;
+  plane.position.y = 0;
+  plane.position.z = z;
+  root.add(plane);
+}
